bd9b9e0d3a2423dc7e352b580d731604d3073dd0
Maciej <maciej.myslinski@netguru.co>
2019-05-31 23:56:05 +0200

    remove default Styles

--- AdvancedGoogleMaps/resources/assets/js/renderMaps.js
+++ AdvancedGoogleMaps/resources/assets/js/renderMaps.js
@@ -3,73 +3,6 @@
 'use strict';
 
 var defaultZoom = 10;
-var defaultStyles = [
-  {
-    featureType: 'all',
-    elementType: 'labels.text.fill',
-    stylers: [{ saturation: 36 }, { color: '#000000' }, { lightness: 40 }],
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.text.stroke',
-    stylers: [{ visibility: 'on' }, { color: '#000000' }, { lightness: 16 }],
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.icon',
-    stylers: [{ visibility: 'off' }],
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#000000' }, { lightness: 20 }],
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#000000' }, { lightness: 17 }, { weight: 1.2 }],
-  },
-  {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 20 }],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 21 }],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#000000' }, { lightness: 17 }],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#000000' }, { lightness: 29 }, { weight: 0.2 }],
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 18 }],
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 16 }],
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 19 }],
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 17 }],
-  },
-];
 
 (function() {
   function advancedGoogleMaps() {
@@ -88,7 +21,6 @@ var defaultStyles = [
       try {
         styles = JSON.parse(mapElement.dataset.styles);
       } catch (error) {
-        styles = defaultStyles;
       }
 
       geocoder.geocode(
@@ -118,7 +50,7 @@ var defaultStyles = [
           zoom: zoom,
           disableDefaultUI: true,
           center: position,
-          styles: styles || defaultStyles,
+          styles: styles,
         };
 
         var map = new google.maps.Map(mapElement, mapOptions);
